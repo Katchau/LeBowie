0be405644e59ae0f1dfc97d0b3c0fc9f2455b1d4
joaosilva22 <j.pedro004@gmail.com>
2017-04-19 15:58:07 +0100

    ...

--- actions/questions/index.php
+++ actions/questions/index.php
@@ -2,11 +2,15 @@
 include_once("../../config/init.php");
 include_once($BASE_DIR . "database/questions.php");
 
+$id = strip_tags($_POST["question-user-id"]);
 $title = strip_tags($_POST["question-title"]);
 $description = strip_tags($_POST["question-description"]);
 $topic = strip_tags($POST["question-topic"]);
-$tags = strip_tags($POST["question-tags"]);
 
-createQuestion(1, $title, $description, 1, array("a", "b", "c"));
+$commaSeparatedTags = strip_tags($POST["question-tags"]);
+$untrimmedTags = explode(",", $commaSeparatedTags);
+$tags = array_map('trim', $untrimmedTags);
+
+createQuestion($id, $title, $description, $topic, $tags);
 header("Location: $BASE_URL" . "pages/questions/ask.php");
 ?>
