e54e61b71831a0d24ba31437faaafa9a8d6322a8
joaosilva22 <j.pedro004@gmail.com>
2017-04-19 15:07:51 +0100

    ...

--- database/posts.php
+++ database/posts.php
@@ -1,25 +1,15 @@
 <?php
 function createPost($userId, $description) {
-	echo "Preparing to insert post";
-
     global $conn;
     $stmt = $conn->prepare("INSERT INTO post (current_state) VALUES (?)");
     $stmt->execute(array("Published"));
-
-	echo "Created post";
 
     $lastId = $conn->lastInsertId();
     $stmt = $conn->prepare("INSERT INTO postinstance (post_id, user_id, description) VALUES (?, ?, ?)");
     $stmt->execute(array($lastId, $userId, $description));
 
-	echo "Created post instance";
-	echo "lastId is " . $lastId;
-
     $stmt = $conn->prepare("INSERT INTO activity (post_id, user_id, action) VALUES (?, ?, ?)");
     $stmt->execute(array($lastId, $userId, "Create"));
-
-	echo "Created activity";
-	echo "Returning postId " . $lastId;
     return $lastId;
 }
 ?>
