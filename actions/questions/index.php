fca00250d665f97035093bdcca4c9376ee182da9
joaosilva22 <j.pedro004@gmail.com>
2017-04-19 16:19:57 +0100

    ...

--- actions/questions/index.php
+++ actions/questions/index.php
@@ -8,18 +8,9 @@ $description = strip_tags($_POST["question-description"]);
 $topic = strip_tags($_POST["question-topic"]);
 
 $commaSeparatedTags = strip_tags($_POST["question-tags"]);
-echo "CommaSeparatedTags=$commaSeparatedtags";
-
 $untrimmedTags = explode(",", $commaSeparatedTags);
-foreach ($untrimmedTags as $untrimmedTag) {
-	echo "UntrimmedTag=$untrimmedTag";
-}
-
 $tags = array_map('trim', $untrimmedTags);
-foreach ($tags as $tag) {
-	echo "Tag=$tag"; 
-}
 
 createQuestion($id, $title, $description, $topic, $tags);
-//header("Location: $BASE_URL" . "pages/questions/ask.php");
+header("Location: $BASE_URL" . "pages/questions/ask.php");
 ?>
