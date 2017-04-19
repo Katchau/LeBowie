66264f9c890eb828687ba86ecb694a70dbc116c3
joaosilva22 <j.pedro004@gmail.com>
2017-04-19 15:36:56 +0100

    ...

--- pages/questions/ask.php
+++ pages/questions/ask.php
@@ -1,5 +1,9 @@
 <?php
-	include_once('../../config/init.php');
-	$smarty->display('questions/ask.tpl');
+include_once('../../config/init.php');
+include_once($BASE_DIR . "database/topic.php");
 
+$topics = getTopics();
+	
+$smarty->assign('topics', $topics);
+$smarty->display('questions/ask.tpl');
 ?>
--- pages/questions/landing.php
+++ pages/questions/landing.php
@@ -1,15 +1,13 @@
 <?php
-
-	include_once('../../config/init.php');
-	include_once($BASE_DIR . 'actions/questions/question.php');
+include_once('../../config/init.php');
+include_once($BASE_DIR . 'actions/questions/question.php');
 	
-	$hot_questions = hotQuestions();
-	$recent_questions = recentQuestions();
-	$best_questions = bestQuestions();
+$hot_questions = hotQuestions();
+$recent_questions = recentQuestions();
+$best_questions = bestQuestions();
 	
-	$smarty->assign('hquestions',$hot_questions);
-	$smarty->assign('rquestions',$recent_questions);
-	$smarty->assign('bquestions',$best_questions);
-	$smarty->display('frontpage/landing.tpl');
-
+$smarty->assign('hquestions',$hot_questions);
+$smarty->assign('rquestions',$recent_questions);
+$smarty->assign('bquestions',$best_questions);
+$smarty->display('frontpage/landing.tpl');
 ?>
