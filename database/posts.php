144276580a170c72bd225f81ae46fe0762a74edc
joaosilva22 <j.pedro004@gmail.com>
2017-04-19 15:01:49 +0100

    ...

--- database/posts.php
+++ database/posts.php
@@ -13,6 +13,7 @@ function createPost($userId, $description) {
     $stmt->execute(array($lastId, $userId, $description));
 
 	echo "Created post instance";
+	echo "lastId is " . $lastId;
 
     $stmt->$conn->prepare("INSERT INTO activity (post_id, user_id, action) VALUES (?, ?, ?)");
     $stmt->execute(array($lastId, $userId, "Create"));
