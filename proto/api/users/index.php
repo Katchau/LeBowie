6c4aef38a7543f09a4553eab3af978250607efba
joaosilva22 <j.pedro004@gmail.com>
2017-04-20 09:37:24 +0100

    ...

--- proto/api/users/index.php
+++ proto/api/users/index.php
@@ -2,10 +2,31 @@
 include_once("../../config/init.php");
 include_once($BASE_DIR . "database/users.php");
 
-$user = getUserById($_GET["id"]);
+$id = $_GET["id"];
+$user = getUserById($id);
+$badges = getUserBadges($id);
+
+$badgesObject = [];
+foreach ($badges as $badge) {
+    $badgesObject[] = [
+	"id" => $badge["id"],
+	"color" => $badge["color"],
+	"text" => $badge["text"]
+    ];
+}
+    
 $userObject = [
     "user_id" => $user["id"],
-    "username" => $user["username"]
+    "username" => $user["username"],
+    "email" => $user["email"],
+    "first_name" => $user["first_name"],
+    "last_name" => $user["last_name"],
+    "country" => $user["country"],
+    "description" => $user["description"],
+    "image" => $user["image"],
+    "score" => $user["score"],
+    "badges" => $badgesObject
 ];
+
 echo json_encode($userObject);
 ?>
