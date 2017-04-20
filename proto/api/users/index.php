2a1ecfddc453aea88ae809da469c66c8de259205
joaosilva22 <j.pedro004@gmail.com>
2017-04-20 09:28:01 +0100

    ...

--- proto/api/users/index.php
+++ proto/api/users/index.php
@@ -3,6 +3,9 @@ include_once("../../config/init.php");
 include_once($BASE_DIR . "database/users.php");
 
 $user = getUserById($_GET["id"]);
-
+$user_object = [
+    "user_id" => $user.id,
+    "username" => $user.username
+];
 echo json_encode($user);
 ?>
