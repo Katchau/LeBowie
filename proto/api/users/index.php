d25ace53cf7b3340bc0d8f0d839650869adbf3f2
joaosilva22 <j.pedro004@gmail.com>
2017-04-20 09:21:38 +0100

    ...

--- proto/api/users/index.php
+++ proto/api/users/index.php
@@ -4,9 +4,5 @@ include_once($BASE_DIR . "database/users.php");
 
 $user = getUserById($_GET["id"]);
 
-if ($user->rowCount() == 0) {
-    http_response_code(404);
-} else {
-    echo json_encode($user);
-}
+echo json_encode($user);
 ?>
