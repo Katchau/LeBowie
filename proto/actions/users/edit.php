576b60777b18a61d72c4fd5dce3e9dc6c2910016
joaosilva22 <j.pedro004@gmail.com>
2017-04-24 15:25:42 +0100

    ...

--- proto/actions/users/edit.php
+++ proto/actions/users/edit.php
@@ -9,15 +9,12 @@ $firstName = postValueOrNull('first_name');
 $lastName = postValueOrNull('last_name');
 $description = postValueOrNull('description');
 
-echo "The new description is $description";
-
 updateUser($id, $email, $password, $firstName, $lastName, $description);
-//header('Location: ' . $BASE_URL . 'pages/users/?username=' . $_SESSION['username']);
+header('Location: ' . $BASE_URL . 'pages/users/?username=' . $_SESSION['username']);
 
 function postValueOrNull($name)
 {
     $value = strip_tags($_POST[$name]);
-    echo "The value for $name is $value";
     if (!isset($value) || trim($value) == NULL) {
         return NULL;
     }
