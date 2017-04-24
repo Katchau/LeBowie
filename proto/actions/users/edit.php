4bbba1243e194246d4e1bb65403cb7d58e841103
joaosilva22 <j.pedro004@gmail.com>
2017-04-24 15:02:03 +0100

    ...

--- proto/actions/users/edit.php
+++ proto/actions/users/edit.php
@@ -3,14 +3,21 @@ require_once '../../config/init.php';
 require_once $BASE_DIR . 'database/users.php';
 
 $id = $_SESSION['userid'];
-isset($_POST['email']) ? ($email = $_POST['email']) : ($email = NULL);
-isset($_POST['password']) ? ($password = $_POST['password']) : ($password = NULL);
-isset($_POST['first_name']) ? ($firstName = $_POST['first_name']) : ($firstName = NULL);
-isset($_POST['last_name']) ? ($lastName = $_POST['last_name']) : ($lastName = NULL);
-isset($_POST['description']) ? ($description = $_POST['description']) : ($description = $_POST['description']);
-
-echo "First name is $firstName\n";
+$email = postValueOrNull('email');
+$password = postValueOrNull('password');
+$firstName = postValueOrNull('first_name');
+$lastName = postValueOrNull('last_name');
+$description = postValueOrNull('description');
 
 updateUser($id, $email, $password, $firstName, $lastName, $description);
-//header('Location: ' . $BASE_URL . 'pages/users/?id=' . $id);
+header('Location: ' . $BASE_URL . 'pages/users/?id=' . $id);
+
+function postValueOrNull($name)
+{
+    $value = $_POST[$name];
+    if (isset($value) || trim($value) == NULL) {
+        return NULL;
+    }
+    return $value;
+}
 ?>
