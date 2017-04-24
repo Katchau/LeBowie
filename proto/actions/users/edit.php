d4cc98a634882b630b0906eb6abd51fe8c82223e
joaosilva22 <j.pedro004@gmail.com>
2017-04-24 14:49:11 +0100

    ...

--- proto/actions/users/edit.php
+++ proto/actions/users/edit.php
@@ -3,13 +3,13 @@ require_once '../../config/init.php';
 require_once $BASE_DIR . 'database/users.php';
 
 $id = $_SESSION['userid'];
-isset($_POST['email']) ? $email = $_POST['email'] : $email = NULL;
+isset($_POST['email']) ? ($email = $_POST['email']) : ($email = NULL);
 $password = $_POST['password'];
 $firstName = $_POST['first_name'];
 $lastName = $_POST['last_name'];
 $description = $_POST['description'];
 
-if ($email === NULL) {
+if (!$email) {
     echo "e-mail is null";
 }
 
