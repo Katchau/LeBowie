7d5e06128e0ac6b7254d8beb75c16776c3abc5e6
joaosilva22 <j.pedro004@gmail.com>
2017-04-24 14:37:00 +0100

    ...

--- proto/actions/users/edit.php
+++ proto/actions/users/edit.php
@@ -3,6 +3,7 @@ require_once '../../config/init.php';
 require_once $BASE_DIR . 'database/users.php';
 
 $id = $_SESSION['id'];
+echo $id;
 $email = $_POST['email'];
 $password = $_POST['password'];
 $firstName = $_POST['first_name'];
@@ -10,5 +11,5 @@ $lastName = $_POST['last_name'];
 $description = $_POST['description'];
 
 updateUser($id, $email, $password, $firstName, $lastName, $description);
-header('Location: ' . $BASE_URL . '/pages/users/?id=' . $id);
+//header('Location: ' . $BASE_URL . 'pages/users/?id=' . $id);
 ?>
