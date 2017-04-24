e76ffe8a3123375326cecbfe1a24376b2ac0fb9a
RuiCS <rui.pedro.csoares@gmail.com>
2017-04-24 16:44:23 +0100

    oops

--- proto/actions/answers/index.php
+++ proto/actions/answers/index.php
@@ -4,7 +4,7 @@ require_once "../../config/init.php";
 require_once $BASE_DIR . "database/questions.php";
 
 $id = strip_tags($_POST["answer-user-id"]);
-$question_id = strip_tags("answer-question-id");
+$question_id = strip_tags($_POST["answer-question-id"]);
 $description = strip_tags($_POST["answer-body"]);
 
 //createAnswer($id, $question_id, $description);
