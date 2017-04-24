3f90d1d53a5b2a0901446ede5f1c0e92eff80389
joaosilva22 <j.pedro004@gmail.com>
2017-04-24 18:04:16 +0100

    ...

--- proto/javascript/questions.js
+++ proto/javascript/questions.js
@@ -3,7 +3,7 @@ const BASE_URL = window.location.origin + '/~lbaw1651/proto/';
 $(function() {
     let questionId = parseInt($('#question-id').val());
     $('#upvote-btn').click(function() {
-        $.post(`${BASE_URL}api/questions/upvote.php`, { 'id': 282 });    
+        $.post(`${BASE_URL}api/questions/upvote.php`, { 'id': 282 }, "json");    
     });
     $('#downvote-btn').click(function() {
         $.post(`${BASE_URL}api/questions/downvote.php`, { id: questionId });
