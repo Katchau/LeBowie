d0885f9255f52b5f79deeb92a6e5b37e6d7d6636
joaosilva22 <j.pedro004@gmail.com>
2017-04-24 15:27:17 +0100

    ...

--- proto/database/users.php
+++ proto/database/users.php
@@ -145,7 +145,7 @@ function updateUser($userId, $email, $password, $firstName, $lastName, $descript
         updateUserLastName($userId, $lastName);
     }
     if ($description != NULL) {
-        updateUserDescriptin($userId, $description);
+        updateUserDescription($userId, $description);
     }
 }
 
