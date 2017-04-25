ce4538f0cac8f95a951c160e2228989bbb85370e
joaosilva22 <j.pedro004@gmail.com>
2017-04-25 11:57:25 +0100

    ...

--- proto/javascript/questions.js
+++ proto/javascript/questions.js
@@ -2,19 +2,34 @@ const BASE_URL = window.location.origin + '/~lbaw1651/proto/';
 
 $(function() {
     let questionId = $('#question-id').val();
-    $('#upvote-btn').click(function() {
-        $.post(`${BASE_URL}api/questions/upvote.php`, 
-        { 
-            'id': questionId 
-        }, function(data, status) {
-            if (status == 'success') {
-                let upvoteElem = $('#upvote-btn span');
-                let upvotes = parseInt(upvoteElem.text());
-                upvoteElem.text(upvotes + 1);
-            }
-        });    
-    });
-    $('#downvote-btn').click(function() {
-        $.post(`${BASE_URL}api/questions/downvote.php`, { 'id': questionId });
-    });
+    $('#upvote-btn').click(upvoteQuestion(questionId));
+    $('#downvote-btn').click(downvoteQuestion(questionId));
 });
+
+function upvoteQuestion(questionId) {
+    $.post(`${BASE_URL}api/questions/upvote.php`),
+    {
+        id: questionId
+    },
+    function(data, status) {
+        if (status == 'success') {
+            let upvotebtn = $('#js-upvote-btn span');
+            let upvotes = parseInt(upvotebtn.text());
+            upvotebtn.text(upvotes + 1);
+        }
+    }
+}
+
+function downvoteQuestion(questionId) {
+    $.post(`${BASE_URL}api/questions/downvote.php`),
+    {
+        id: questionId
+    },
+    function(data, status) {
+        if (status == 'success') {
+            let downvotebtn = $('#js-downvote-btn span');
+            let downvotes = parseInt(downvotebtn.text());
+            downvotebtn.text(upvotes + 1);
+        }
+    }
+}
