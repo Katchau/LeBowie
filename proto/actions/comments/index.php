7a97ff97b066c4573c34af5c8d253870be232eae
RuiCS <rui.pedro.csoares@gmail.com>
2017-04-26 16:52:14 +0100

    oops

--- proto/actions/comments/index.php
+++ proto/actions/comments/index.php
@@ -8,6 +8,7 @@ require_once $BASE_DIR . "database/comments.php";
 $id = strip_tags($_POST["comment-user-id"]);
 $answer_id = strip_tags($_POST["comment-answer-id"]);
 $description = strip_tags($_POST["comment-body"]);
+$question_id = strip_tags($_POST["comment-question-id"]);
 
 createComment($id, $answer_id, $description);
 header("Location: $BASE_URL" . "pages/questions/index.php?id=" . $question_id);
