723e613426193add7fe320825d9041332c13ae26
RuiCS <rui.pedro.csoares@gmail.com>
2017-04-26 12:19:52 +0100

    ....

--- proto/pages/answers/comment.php
+++ proto/pages/answers/comment.php
@@ -7,6 +7,9 @@ require_once $BASE_DIR .'database/answers.php';
 $answerId = $_GET["id"];
 $questionId = getQuestionFromAnswer($answerId);
 
+echo "QuestioID: " . $questionId . "\n";
+echo "QuestionID[0] " . $questionId[0] . "\n";
+
 $question = getQuestionInfo($questionId[0]);
 $tags = getQuestionTags($questionId);
 $answer = getAnswerInfo($answerId);
