c400087b571439752cb840dc0ec903eabfcd0692
RuiCS <rui.pedro.csoares@gmail.com>
2017-05-02 17:01:13 +0100

    reports

---
+++ proto/database/reports.php
@@ -0,0 +1,34 @@
+<?php
+
+function createReport($post_id, $user_id, $title, $content, $reason, $created){
+
+    global $conn;
+
+    try {
+        $conn->beginTransaction();
+        $stmt = $conn->prepare("INSERT INTO Report (post_id, user_id, title, content, reason, created) VALUES (?,?,?,?,?,?) ");
+        $stmt->execute(array($post_id, $user_id, $title, $content, $reason, $created));
+        $conn->commit();
+
+    } catch (PDOException $e){
+        $conn->rollBack();
+        echo 'Failed: ' . $e->getMessage();
+        return 0;
+    }
+
+}
+
+function deleteReport($reportId){
+    global $conn;
+    $stmt = $conn->prepare("DELETE FROM report WHERE id = ?");
+    $stmt->execute(array($reportId));
+}
+
+function getReport($post_id, $user_id){
+    global $conn;
+    $stmt = $conn->prepare("SELECT id, title, content, reason, created FROM report WHERE post_id = ? AND user_id = ?");
+    $stmt->execute(array($post_id, $user_id));
+    return $stmt->fetchAll();
+}
+
+ ?>
