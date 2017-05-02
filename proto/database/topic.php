9dec97da59339701f08281c16e4fd9b8ff46ba72
RuiCS <rui.pedro.csoares@gmail.com>
2017-05-02 16:19:16 +0100

    R504 R505 R506

--- proto/database/topic.php
+++ proto/database/topic.php
@@ -1,5 +1,5 @@
 <?php
-function getTopics() 
+function getTopics()
 {
     global $conn;
     $stmt = $conn->prepare("SELECT id, topicname FROM topic");
@@ -22,4 +22,57 @@ function getTopicInfo($topicId)
     $stmt->execute(array($topicId));
     return $stmt->fetch();
 }
+
+function createTopic($admin_id, $topicname, $description){
+    global $conn;
+
+    try {
+        $conn->beginTransaction();
+        $stmt = $conn->prepare("INSERT INTO Topic (admin_id, topicname, description) VALUES (?,?,?) ");
+        $stmt->execute(array($admin_id, $topicname, $description));
+        $conn->commit();
+
+    } catch (PDOException $e){
+        $conn->rollBack();
+        echo 'Failed: ' . $e->getMessage();
+        return 0;
+    }
+}
+
+function deleteTopic($topicId){
+    global $conn;
+    $stmt = $conn->prepare("DELETE FROM Topic WHERE id = ?");
+    $stmt->execute(array($topicId));
+}
+
+function updateTopicName($topicId, $topicName){
+    global $conn;
+    $stmt = $conn->prepare('UPDATE topic SET topicname = ? WHERE id = ?');
+    $stmt->execute(array($topicName, $topicId));
+}
+
+function updateTopicDescription($topicId, $topicDescription){
+    global $conn;
+    $stmt = $conn->prepare('UPDATE topic SET description = ? WHERE id = ?');
+    $stmt->execute(array($topicName, $topicId));
+}
+
+function updateTopic($topicId, $newName, $newDescription){
+
+    if ($topicId != NULL) {
+        if ($nameName != NULL) {
+            updateTopicName($topicId, $newName);
+        }
+        if ($newDescription != NULL){
+            updateTopicDescription($topicId, $newDescription);
+        }
+    }
+    else {
+        echo 'Failed update';
+        return 0;
+    }
+}
+
+
+
 ?>
