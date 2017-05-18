04f418d9be0b3d91fd7af4f664efc7af8f32e7ae
joaosilva22 <j.pedro004@gmail.com>
2017-05-18 10:14:51 +0100

    Add error and success messages to reports

--- final/actions/reports/index.php
+++ final/actions/reports/index.php
@@ -10,6 +10,11 @@ $title = strip_tags($_POST["report-title"]);
 $reason = strip_tags($_POST["report-reason"]);
 $content = strip_tags($_POST["report-content"]);
 
-createReport($postId, $userId, $title, $content, $reason);
+try {
+    createReport($postId, $userId, $title, $content, $reason);
+    $_SESSION["success_messages"][] = "Report created successfuly";
+} catch (PDOException $e) {
+    $_SESSION["error_messages"][] = "Report creation failed";
+}
 header("Location: $BASE_URL" . "pages/questions/index.php?id=$questionId");
 ?>
