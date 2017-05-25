d573b6dae6d3010b3d9449d24c7d34eb6e0c48f5
joaosilva22 <j.pedro004@gmail.com>
2017-05-25 12:16:01 +0100

    ...

--- final/javascript/google-login/google-login.js
+++ final/javascript/google-login/google-login.js
@@ -7,8 +7,8 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
     document.getElementById('google-signin-email').value = profile.getEmail(); 
-    document.getElementById('google-signin-first-name').value = profile.getFirstName();
-    document.getElementById('google-signin-last-name').value = profile.getLastName();
+    document.getElementById('google-signin-first-name').value = profile.getGivenName();
+    document.getElementById('google-signin-last-name').value = profile.getFamilyName();
     document.getElementById('google-signin-image').value = profile.getImageUrl();
 
     //document.getElementById('google-signin').submit();
