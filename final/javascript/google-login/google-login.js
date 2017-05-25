720dd9841c5adf73ea862fa47a8dd08b910ae9b2
joaosilva22 <j.pedro004@gmail.com>
2017-05-25 14:41:09 +0100

    ...

--- final/javascript/google-login/google-login.js
+++ final/javascript/google-login/google-login.js
@@ -10,9 +10,13 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-        window.location.href= `${BASE_URL}actions/users/logout.php`;
+    gapi.load(auth2, function () {
+        gapi.auth2.init().then(function () {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+                console.log('User signed out.');
+                window.location.href = `${BASE_URL}actions/users/logout.php`;
+            });
+        });
     });
 }
