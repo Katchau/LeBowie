96527fe8dbab152acf844b19cf2a436382919fb2
joaosilva22 <j.pedro004@gmail.com>
2017-05-25 11:33:52 +0100

    ...

--- final/javascript/google-login/google-login.js
+++ final/javascript/google-login/google-login.js
@@ -6,7 +6,7 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-    $.post(`${BASE_URL}pages/signup-google.php`, {
+    $.post(`${BASE_URL}pages/users/signup-google.php`, {
         email: profile.getEmail(),
         first_name: profile.getGivenName(),
         last_name: profile.getFamilyName(),
