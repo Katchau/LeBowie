11c31a599ea05453d55d1f65179c2e64feac77e1
joaosilva22 <j.pedro004@gmail.com>
2017-05-25 14:41:52 +0100

    ...

--- final/javascript/google-login/google-login.js
+++ final/javascript/google-login/google-login.js
@@ -10,7 +10,7 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
-    gapi.load(auth2, function () {
+    gapi.load('auth2', function () {
         gapi.auth2.init().then(function () {
             var auth2 = gapi.auth2.getAuthInstance();
             auth2.signOut().then(function () {
