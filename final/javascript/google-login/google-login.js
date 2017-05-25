95d00347bca4310b8549f159ea21f6e0a0440ae5
joaosilva22 <j.pedro004@gmail.com>
2017-05-25 12:34:52 +0100

    ...

--- final/javascript/google-login/google-login.js
+++ final/javascript/google-login/google-login.js
@@ -1,12 +1,7 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getGivenName());
-    console.log('Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-    document.getElementById('google-signin-email').value = profile.getEmail(); 
+    document.getElementById('google-signin-email').value = profile.getEmail();
     document.getElementById('google-signin-first-name').value = profile.getGivenName();
     document.getElementById('google-signin-last-name').value = profile.getFamilyName();
     document.getElementById('google-signin-image').value = profile.getImageUrl();
@@ -18,5 +13,23 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+    });
+}
+
+function onLoad() {
+    gapi.load('auth2,signin2', function () {
+        var auth2 = gapi.auth2.init();
+        auth2.then(function () {
+            // Current values
+            var isSignedIn = auth2.isSignedIn.get();
+            var currentUser = auth2.currentUser.get();
+
+            if (!isSignedIn) {
+                // Rendering g-signin2 button.
+                gapi.signin2.render('google-signin-button', {
+                    'onsuccess': 'onSignIn'
+                });
+            }
+        });
     });
 }
