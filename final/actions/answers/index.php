54ed4b5ea7801ce35cb0942eb2c949e4fbbff727
Jonas L <joaohccl@live.com.pt>
2017-05-25 15:26:24 +0100

    ay lmao

--- final/actions/answers/index.php
+++ final/actions/answers/index.php
@@ -8,7 +8,15 @@ $id = strip_tags($_POST["answer-user-id"]);
 $question_id = strip_tags($_POST["answer-question-id"]);
 $description = strip_tags($_POST["answer-body"]);
 
+if (!$_POST['answer-body'] || $description == "") {
+    $_SESSION['error_messages'][] = "Anwser body can't be empty!";
+    $_SESSION['form_values'] = $_POST;
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    exit;
+}
+
 createAnswer($id, $question_id, $description);
 header("Location: $BASE_URL" . "pages/questions/index.php?id=" . $question_id);
+
 
 ?>
