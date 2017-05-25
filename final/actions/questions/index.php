ce0fb203e61beab276214bc92f426e1dc61ac13f
joaosilva22 <j.pedro004@gmail.com>
2017-05-25 15:32:33 +0100

    ...

--- final/actions/questions/index.php
+++ final/actions/questions/index.php
@@ -4,7 +4,7 @@ require_once $BASE_DIR . "database/questions.php";
 
 $id = strip_tags($_POST["question-user-id"]);
 $title = strip_tags($_POST["question-title"]);
-$description = strip_tags($_POST["question-description"]);
+$description = $_POST["question-description"];
 $topic = strip_tags($_POST["question-topic"]);
 
 $commaSeparatedTags = strip_tags($_POST["question-tags"]);
