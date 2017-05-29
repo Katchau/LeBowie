7a015543b935256c04d7d4227502a42a161856fa
RuiCS <rui.pedro.csoares@gmail.com>
2017-05-29 22:25:21 +0100

    asd

--- final/actions/questions/edit.php
+++ final/actions/questions/edit.php
@@ -21,9 +21,8 @@ try{
     $userId = postValueOrNull('userId');
     $questionTopic = getTopicByName(postValueOrNull('questionTopic'));
 
-	// $id, $userId, $newTitle, $newDesc, $topic
-    $newId = updateQuestion($id, $userId, $questiontitle, $questiondescription, $questionTopic);
-    header('Location: ' . $BASE_URL . 'pages/questions/index.php?id=' . $newId);
+    //$newId = updateQuestion($id, $userId, $questiontitle, $questiondescription, $questionTopic);
+    //header('Location: ' . $BASE_URL . 'pages/questions/index.php?id=' . $newId);
 }
 catch(Exception $e){
     $_SESSION['error_messages'][] = 'Internal server error, try again later.';
