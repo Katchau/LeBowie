b3ea1ca25f40d3cc6d9e95f2b15af06029c638e9
RuiCS <rui.pedro.csoares@gmail.com>
2017-05-29 22:34:14 +0100

    asd

--- final/actions/questions/edit.php
+++ final/actions/questions/edit.php
@@ -21,7 +21,7 @@ try{
     $userId = postValueOrNull('userId');
     $questionTopic = getTopicByName(postValueOrNull('questionTopic'));
 
-    echo $id . ";" . $questiontitle . ";" . $questiondescription . ";" . $userId . ";" . $questionTopic['topicname'];
+    echo $id . ";" . $questiontitle . ";" . $questiondescription . ";" . $userId . ";" . implode("|",$questionTopic);;
     //$newId = updateQuestion($id, $userId, $questiontitle, $questiondescription, $questionTopic);
     //header('Location: ' . $BASE_URL . 'pages/questions/index.php?id=' . $newId);
 }
