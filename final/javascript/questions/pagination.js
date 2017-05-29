2c1adb36e26c922f949c013bfa5b55bdf7971061
joaosilva22 <j.pedro004@gmail.com>
2017-05-29 10:26:07 +0100

    ...

--- final/javascript/questions/pagination.js
+++ final/javascript/questions/pagination.js
@@ -4,10 +4,11 @@ let offset = 0;
 function getMoreHotQuestions() {
     $.get(`${BASE_URL}api/questions/hot-questions.php?offset=${offset}&limit=${limit}`, function(data) {
         offset += limit;
-        console.log(data);
 
         data.forEach(function(element) {
             $('#hotq').append(`<div>${element.title}<div>`);
         }, this);
+
+        $('#view-more').detach().appendTo('#hotq');
     }, 'json');
 }
