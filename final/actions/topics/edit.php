0af4937bc000313fa51606d5bffebb5f451184f1
RuiCS <rui.pedro.csoares@gmail.com>
2017-05-29 14:01:59 +0100

    asd

--- final/actions/topics/edit.php
+++ final/actions/topics/edit.php
@@ -12,17 +12,19 @@ function postValueOrNull($name)
     return $value;
 }
 
-try{
+
 
     $id = postValueOrNull('id');
     $topicname = postValueOrNull('topicname');
     $description = postValueOrNull('topicdescription');
+
+try{
 	
     updateTopic($id, $topicname, $description);
     header('Location: ' . $BASE_URL . 'pages/topic/list?id=' . $id);
 }
 catch(Exception $e){
-    $_SESSION['error_messages'][] = 'Internal server error, try again later.';
+    $_SESSION['error_messages'][] = 'Internal server error, try again later.' . $id . ";" . $topicname ";" . $description;
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 ?>
