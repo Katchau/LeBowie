b1cdba2dfd4effc6a87fc68df1d692e798b30bae
joaosilva22 <j.pedro004@gmail.com>
2017-05-29 17:56:58 +0100

    ...

---
+++ final/api/answers/unaccept.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../../config/init.php';
+require_once $BASE_DIR . 'database/answers.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$params = json_decode(file_get_contents('php://input'), true);
+switch ($method) {
+    case 'GET':
+        get($_GET);
+        break;
+    default:
+        http_response_code(405);
+}
+
+function get($params)
+{
+    $id = $_GET["id"];
+    unacceptAnswer($id);
+}
+?>
